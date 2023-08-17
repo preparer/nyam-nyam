8088c1784d2495d5db36a3f1fdcc41715abf253c
Eldana <fineshine6718@gmail.com>
2023-08-17 20:02:20 +0600

    new image in home

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import back from '../../assets/orange.jpg'
+import back from '../../assets/orange_auto_x2_colored_toned_light_ai.jpg'
 import s from './Home.module.scss'
 
 function HomePage() {
